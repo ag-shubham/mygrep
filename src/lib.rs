650c5eebe226c57aa642ccb65f49a1ff934643bd
ag-shubham <shubhamsatnalika64@gmail.com>
2024-12-09 16:38:12 +0530

    added: search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,55 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query: query,
+            filename: filename,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let results = contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect();
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_config() {
+        let args = vec![
+            String::from("mygrep"),
+            String::from("query"),
+            String::from("filename"),
+        ];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, "query");
+        assert_eq!(config.filename, "filename");
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
