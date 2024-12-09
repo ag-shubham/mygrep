a654104b2d4123b285490be6e36497c41f3e357b
ag-shubham <shubhamsatnalika64@gmail.com>
2024-12-09 15:49:55 +0530

    reading and processing input file and the key to be searched

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use mygrep::{run, Config};
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let result = run(config);
+    if let Err(e) = result {
+        println!("Error in Reading file: {}", e);
+        process::exit(1);
+    }
 }
